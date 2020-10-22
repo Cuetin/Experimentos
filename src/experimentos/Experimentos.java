30c7789720189ed11b8b26b08027fde7386e5cf0
Cuetin <josecuetin@gmail.com>
2020-10-22 16:37:20 +0200

    Cambio metallica

--- src/experimentos/Experimentos.java
+++ src/experimentos/Experimentos.java
@@ -16,7 +16,7 @@ public class Experimentos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Primer commit master");
+        System.out.println(" Heavy metal!!!!!! Viva Metallica");
     }
     
 }
